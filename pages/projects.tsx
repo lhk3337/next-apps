1e10172f724ca86dfc65828b665f7596a502664a
lhk3337 <lhk3337@naver.com>
2022-06-22 22:07:04 +0900

    [FEAT] name display

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,9 +2,13 @@ import Layout from "components/layout";
 import { DATABASE_ID, TOKKEN } from "config";
 
 export default function Projects({ projects }: any) {
+  // console.log(projects);
   return (
     <Layout>
       <h1>총 프로젝트 {projects.results.length}</h1>
+      {projects.results.map((project: any,index:number) => (
+         <h1 key={index}>{project.properties["이름"].title[0].plain_text}</h1> 
+      ))} 
     </Layout>
   );
 }
@@ -18,12 +22,14 @@ export async function getStaticProps() {
       "Content-Type": "application/json",
       Authorization: `Bearer ${TOKKEN}`,
     },
-    body: JSON.stringify({ page_size: 100 }),
+    body: JSON.stringify({
+      page_size: 100,
+    }),
   };
 
   const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
   const projects = await res.json();
-
+console.log(projects)
   return {
     props: { projects },
   };
